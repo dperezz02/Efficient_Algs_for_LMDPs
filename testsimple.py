6fc0520b51a9f2ada72887c2a690cdc93cf9b484
anders.jonsson@upf.edu <anders.jonsson@upf.edu>
2024-03-14 09:26:28 +0100

    simple grid

--- testsimple.py
+++ testsimple.py
@@ -2,7 +2,9 @@ from environments.simplegrid import SimpleGrid
 
 if __name__ == "__main__":
     g = SimpleGrid(2)
-    Z, nsteps = SimpleGrid.power_iteration(g, 1)
+    Z, nsteps = g.power_iteration(1, 1e-10)
     print(Z)
     
-
+    mdp = g.embedding_to_MDP()
+    Q, pi, n = mdp.value_iteration(1e-10, 1)
+    print(Q)
